e7fd96d902caff1e41921baaac5fee85fa8bcba6
Aparecido Silvano <silvanoalbuquerque@gmail.com>
2016-06-02 12:42:37 -0300

    melhorando nomes de variavéis.

--- pip/mini-projeto-pip/news_app.py
+++ pip/mini-projeto-pip/news_app.py
@@ -6,25 +6,25 @@ from db import noticias
 app = Flask('wtf', static_folder='assets')
 
 
-@app.route("/noticias/cadastro", methods=["GET", "POST"])
+@app.route("/tarefas/cadastro", methods=["GET", "POST"])
 def cadastro():
     if request.method == "POST":
         dados_do_formulario = request.form.to_dict()
-        id_nova_noticia = noticias.insert(dados_do_formulario)
-        return render_template('cadastro_sucesso.html', id_nova_noticia=id_nova_noticia)
-	
+        id_nova_atividade = noticias.insert(dados_do_formulario)
+        return render_template('cadastro_sucesso.html', id_nova_atividade=id_nova_atividade)
+
     else:  # GET
         return render_template('cadastro.html', title=u"Inserir nova noticia")
 
-	
+
 
 @app.route("/")
 def index():
 	todas_as_noticias = noticias.all()
-	return render_template('index.html', noticias=todas_as_noticias, title=u"Todas as noticias")
+	return render_template('index.html', noticias=todas_as_noticias, title=u"Todas as atividades")
 
 
-@app.route("/noticia/<int:noticia_id>")
+@app.route("/atividade/<int:noticia_id>")
 def noticia(noticia_id):
     noticia = noticias.find_one(id=noticia_id)  # query no banco de dados
     return render_template('noticia.html', noticia=noticia)
